06c41de114a0245d12af5ab72851eb60bed556b2
Jared <hustlefirst987@gmail.com>
2024-01-29 23:27:06 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+  ];
+
+  let passwordOne = document.getElementById("pass1");
+  let passwordTwo = document.getElementById("pass2");
+  let passwordLength = 10;
+
+  function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length);
+    return characters[randomChar];
+  }
+
+  function generateRandomPassword() {
+    let generatedPasswordOne = "";
+    let generatedPasswordTwo = "";
+    
+    do {
+      generatedPasswordOne = "";
+      generatedPasswordTwo = "";
+      
+      for (let i = 0; i < passwordLength; i++) {
+        generatedPasswordOne += getRandomCharacter();
+        generatedPasswordTwo += getRandomCharacter();
+      }
+    } while (generatedPasswordOne === generatedPasswordTwo);
+    
+    passwordOne.textContent = generatedPasswordOne;
+    passwordTwo.textContent = generatedPasswordTwo;
+}
